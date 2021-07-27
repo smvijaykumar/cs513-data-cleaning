b6744bf0ea9096dcc8e1d3b42b89389191445a6a
smvijaykumar <vs24@illinois.edu>
2021-07-27 13:13:28 +0530

    update

--- impute_zipcode.py
+++ impute_zipcode.py
@@ -3,16 +3,17 @@ import urllib.request
 import json
 import pandas as pd
 import numpy as np
-df = pd.read_csv("farmersmarkets.csv")
+df = pd.read_csv("farmersmarkets_openrefined.csv")
 
 url = 'https://api.tomtom.com/search/2/reverseGeocode/{geocode}.json?key=Y86v3lNqi3XRTCZoeR616WLfyCSSmg14'
 for index, row in df.iterrows():
-        if ((row['zip'] is np.nan) and not np.isnan(row['y']) and not np.isnan(row['x'])):
-            tobereplaced = str(row['y'])+","+str(row['x'])
+        if ((row['zip'] is np.nan) and not np.isnan(row['Latitude']) and not np.isnan(row['Longitude'])):
+            tobereplaced = str(row['Latitude'])+","+str(row['Longitude'])
             url1 = url.replace("{geocode}",tobereplaced)
+		 print(url1)
             req = urllib.request.urlopen(url1)
             data = req.read().decode('utf-8')
             obj = json.loads(data)
             zip1 = str(obj["addresses"][0]["address"]["postalCode"])
             df.loc[index,'zip']=zip1
-df.to_csv('out1.csv',index=False)
+df.to_csv('farmersmarkets_imputed.csv',index=False)
